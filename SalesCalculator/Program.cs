f5e499a4a6ad8a94e653f083d76201f8dc9b4657
MaruyamaKeito <32102@ojs.ac.jp>
2021-05-28 14:04:47 +0900

    P77 リスト2.26 完成版

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -8,10 +8,9 @@ using System.IO;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(SalesCounter.ReadSales("sales.csv"));
-
-            Dictionary<string,int> amountPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string,int> obj in amountPerStore) {
+            var sales = new SalesCounter("Sales.csv");
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
                 Console.WriteLine("{0} {1}", obj.Key, obj.Value);
             }
         }
