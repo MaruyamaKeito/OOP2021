033ed9e97e2dbed4cac44ad4de0b561f699f3b26
丸山　慶斗 <32102@ojs.ac.jp>
2021-09-13 13:21:46 +0900

    メール送信アプリ作成

---
+++ SendMail/SendMail/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SendMail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+                //アドレス
+                mailMessage.From = new MailAddress("ojsinfosys01@gmail.com");
+                //宛先
+                mailMessage.To.Add(tbTo.Text);
+                //件名
+                mailMessage.Subject = tbTitle.Text;
+                //本文
+                mailMessage.Body = tbMessage.Text;
+
+                //SMTPを使って送信する
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Credentials = new NetworkCredential("ojsinfosys01@gmail.com","Infosys2021");
+                smtpClient.Host = "smtp.gmail.com";
+                smtpClient.Port = 587;
+                smtpClient.EnableSsl = true;
+                smtpClient.Send(mailMessage);
+
+                MessageBox.Show("送信完了");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
