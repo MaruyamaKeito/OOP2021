8ae84c557cb2b4c062bb4ba170478d27d61d4469
丸山　慶斗 <32102@ojs.ac.jp>
2021-09-14 16:02:21 +0900

    0914

--- RssReader/RssReader/Form2.Designer.cs
+++ RssReader/RssReader/Form2.Designer.cs
@@ -29,35 +29,45 @@ namespace RssReader
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btBack = new System.Windows.Forms.Button();
+            this.btAdvance = new System.Windows.Forms.Button();
+            this.wbBrowser = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
-            // button1
+            // btBack
             // 
-            this.button1.Location = new System.Drawing.Point(25, 22);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "戻る";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btBack.Location = new System.Drawing.Point(25, 22);
+            this.btBack.Name = "btBack";
+            this.btBack.Size = new System.Drawing.Size(75, 23);
+            this.btBack.TabIndex = 0;
+            this.btBack.Text = "戻る";
+            this.btBack.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btAdvance
             // 
-            this.button2.Location = new System.Drawing.Point(106, 22);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "進む";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btAdvance.Location = new System.Drawing.Point(106, 22);
+            this.btAdvance.Name = "btAdvance";
+            this.btAdvance.Size = new System.Drawing.Size(75, 23);
+            this.btAdvance.TabIndex = 0;
+            this.btAdvance.Text = "進む";
+            this.btAdvance.UseVisualStyleBackColor = true;
+            // 
+            // wbBrowser
+            // 
+            this.wbBrowser.Location = new System.Drawing.Point(25, 52);
+            this.wbBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbBrowser.Name = "wbBrowser";
+            this.wbBrowser.Size = new System.Drawing.Size(751, 386);
+            this.wbBrowser.TabIndex = 1;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.wbBrowser);
+            this.Controls.Add(this.btAdvance);
+            this.Controls.Add(this.btBack);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
@@ -66,7 +76,8 @@ namespace RssReader
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btBack;
+        private System.Windows.Forms.Button btAdvance;
+        private System.Windows.Forms.WebBrowser wbBrowser;
     }
 }
--- RssReader/RssReader/Form2.cs
+++ RssReader/RssReader/Form2.cs
@@ -1,20 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RssReader
 {
     public partial class Form2 : Form
     {
-        public Form2()
+
+        public Form2(string link)
         {
             InitializeComponent();
+            wbBrowser.Navigate(link);
+        }
+        private void btback_Click(object sender, EventArgs e)
+        {
+            wbBrowser.GoBack();
+        }
+        private void btAdvance_Click(object sender, EventArgs e)
+        {
+            wbBrowser.GoForward();
+        }
+        private void wbBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (wbBrowser.CanGoBack == true)
+            {
+                btBack.Enabled = true;
+            }
+            else
+            {
+                btBack.Enabled = false;
+            }
+            if (wbBrowser.CanGoForward == true)
+            {
+                btAdvance.Enabled = true;
+            }
+            else
+            {
+                btAdvance.Enabled = false;
+            }
         }
     }
 }
